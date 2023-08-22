c80690fa4d2c1acdcb7246df85ff5da4ea26a96f
rehan657 <142786788+rehan657@users.noreply.github.com>
2023-08-22 10:03:50 +0000

    completed

---
+++ voteing.java
@@ -0,0 +1,11 @@
+class voting{
+    public static void main(String[]args){
+    int a=17;
+    if(a>=18){
+        System.out.println("eligible to vote");
+    }
+   else{
+        System.out.println("not eligible to vote");
+ }
+}
+}
